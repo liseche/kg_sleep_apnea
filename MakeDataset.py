9dfbcfa36e3c1bd082f181236c387e712571e35f
Lise Chen <liseche@uio.no>
2024-12-29 10:26:40 +0800

    Restructure folders and dataset script doneish

--- MakeDataset.py
+++ MakeDataset.py
@@ -1,61 +1,81 @@
-"""
-Script to make a dataset for NER processing from free text.
-"""
-
+import spacy
 import argparse
-
+import chardet
+import re
+    
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Make a dataset for NER processing from free text.")
+    parser = argparse.ArgumentParser(description="Make a dataset for (NER) processing from free text. Divides the text using the split strategy and generates a file where each dataset entry (sentence/paragraph) is on one line.")
     
     parser.add_argument(
         '--input_file',
         type=str,
         required=True,
-        help=""
+        help="Path to input file containing free form text you want to transform to a dataset."
     )
 
     parser.add_argument(
         '--output_file',
         type=str,
         default="dataset",
-        help=""
+        help="Name of generated output file containing the dataset."
     )
 
     parser.add_argument(
         '--split_strategy',
         type=str,
-        choices=['sentence', 'paragraph', 'custom'],
+        choices=['sentence', 'paragraph'],
         default='sentence',
-        help=""
+        help="Type of method used to split the free form text for the dataset creation."
     )
-    
-    parser.add_argument(
-        '--max_length',
-        type=int,
-        default=None,
-        help="Maximum character length for each entry."
-    )
+
+    # This one is not implemented. Note to self: tried, but code in thesis_trash.
+    # parser.add_argument(
+    #     '--max_entry_length',
+    #     type=int,
+    #     default=100,
+    #     help="Maximum character length for each entry in the dataset."
+    # )
 
     parser.add_argument(
         '--max_nr_entries',
         type=int,
         default=None,
-        help="Maximum "
-    )
-
-    parser.add_argument(
-        '--tokenize',
-        action='store_true',
-        help=""
+        help="Upper limit for number of entries in the dataset."
     )
 
     args = parser.parse_args()
 
-    if args.verbose:
-        print("Parsed arguments:", args)
-    print(f"Creating dataset with max entry length: {args.max_entry_length}")
+    # print(f"Creating dataset with max entry length: {args.max_entry_length}")
     print(f"Maximum number of entries: {args.max_nr_entries if args.max_nr_entries else 'No limit'}")
-    print(f"Input file: {args.input_file if args.input_file else 'Standard input'}")
+    print(f"Input file: {args.input_file}")
     print(f"Output file: {args.output_file}")
     print(f"Split strategy: {args.split_strategy}")
-    print(f"Tokenization enabled: {args.tokenize}")
+    
+    # Detect the encoding of the file
+    with open(args.input_file, 'rb') as file:
+        raw_data = file.read()
+        encoding = chardet.detect(raw_data)
+        print("Input file encoding: ", encoding['encoding'])
+    
+    with open(args.input_file, mode="r", encoding=encoding['encoding']) as input_file:
+        text = input_file.read()
+        
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    # check for page numbers. Removes some, but not all. Also remove new lines and blank lines in a sentence (unnecessary for dataset)
+    pattern = re.compile(r'\n\s*\d*\s*\n*')
+    sentences = []
+    counter = 0
+    for sent in doc.sents:
+        if args.max_nr_entries and (counter >= args.max_nr_entries):
+            break
+        if sent.text:
+            if pattern.search(sent.text):
+                sentences.append(re.sub(pattern, ' ', sent.text))
+            else:
+                sentences.append(sent.text)
+            counter+=1
+    
+    with open(args.output_file, "w+") as output_file:
+        for sent in sentences:
+            output_file.write(sent + "\n")
---
+++ NER/BioBERT/ner_pytorch.py
@@ -0,0 +1,60 @@
+import argparse
+import torch
+from transformers import AutoModelForTokenClassification, AutoTokenizer, AutoConfig
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def load_model(model_path):
+    tokenizer = AutoTokenizer.from_pretrained(model_path, truncation=True)
+    config = AutoConfig.from_pretrained(model_path)
+    model = AutoModelForTokenClassification.from_config(config)
+    model.to(device)
+    model.eval()
+    return tokenizer, model
+
+def predict_and_save(model, tokenizer, path_to_input : str, path_to_output : str, batch_size : int):
+    id2label = model.config.id2label
+    sentences = get_sentences_from_path(path_to_input)
+
+    batch_sentences = [sentences[i:i + batch_size] for i in range(0, len(sentences), batch_size)]
+    
+    with open(path_to_output, 'w', encoding='utf-8') as file:
+        for batch in batch_sentences:
+            encoding = tokenizer(batch, padding=True, truncation=True, return_tensors="pt", is_split_into_words=True)
+            input_ids, attention_mask = encoding['input_ids'].to(device), encoding['attention_mask'].to(device)
+
+            with torch.no_grad():
+                outputs = model(input_ids, attention_mask=attention_mask)
+                predictions = torch.argmax(outputs.logits, dim=-1)
+            
+            for i, (sentence, preds) in enumerate(zip(batch, predictions)):
+                word_preds = [id2label[predictions.item()] for pred in preds if pred.item() in id2label]
+                for word, label in zip(sentence, word_preds):
+                    file.write(f"{word}\t{label}\n")
+                file.write("\n")
+
+    print(f"NER predictions written to {path_to_output}")
+
+def get_sentences_from_path(file_path : str):
+    sentences = []
+    with open(file_path, 'r', encoding='utf-8') as file:
+        sentence = []
+        for line in file:
+            if line.strip():
+                sentence.append(line.strip())
+            else:
+                if sentence: sentences.append(sentence)
+                sentence = []
+        if sentence: sentences.append(sentence)
+    return sentences
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='NER Prediction Script')
+    parser.add_argument("--model_path", type=str, default="pritamdeka/BioBERT-mnli-snli-scinli-scitail-mednli-stsb", help="path to trained NER model")
+    parser.add_argument("--input_file", type=str, default="../../data/icsd3.tsv", help="path to tab-separated input data file")
+    parser.add_argument("--output_file", type=str, default="predictions.tsv", help="path to generated output predictions")
+    parser.add_argument("--batch_size", type=int, default=32, help="size of batches to process input data in, for parallelization")
+    args = parser.parse_args()
+    tokenizer, model = load_model(args.model_path)
+
+    predict_and_save(model, tokenizer, args.input_file, args.output_file, args.batch_size)
